970a92f6f344e5ece6efeef7993510c3a7df7d9a
Mona-PC\Mona <monjoel_11@yahoo.com>
2017-04-06 01:19:56 +0800

    Almost done but having some problem.. I will try to finish it tomorrow..

--- static/js/imports.js
+++ static/js/imports.js
@@ -31,6 +31,92 @@ function addInstructor() {
     showLatestJson();
 
 }
+//CLASSSES MON
+function showRegisteredIns() {
+    let ins = JSON.parse(localStorage.getItem("instructorList"));
+    window.alert(ins.instructors[0].name);
+    /*for (let i = 0; i < ins.length; i++) {
+     document.innerHTML(ins.instructors[i].name);
+     let op = document.getElementsByName("RegIns").appendChild(document.createElement("option"));
+     }*/
+}
+
+function addClasses() {
+    let classes = localStorage.getItem("classesList");
+
+    let elements = document.getElementById("importClasses").elements;
+    let jsonString = "";
+    for (let i = 0; i < elements.length; i++) {
+        let item = elements.item(i);
+        let name = item.name;
+
+        if (name != undefined && name != "add" && name != "submit") {
+            jsonString = jsonString.concat('"' + item.name + '": "' + item.value + '"');
+            jsonString = jsonString.concat(name != "Day" ? "," : "");
+        }
+    }
+
+    let obj;
+    if (classes == undefined || classes == null) {
+        obj = JSON.parse('{"classes": [{' + jsonString + '}]}');
+    } else {
+        obj = JSON.parse(classes);
+        let t = JSON.parse('{' + jsonString + '}');
+        obj['classes'].push(t);
+    }
+    showClassJson();
+    let jsonStr = JSON.stringify(obj, null, 4);
+    localStorage.setItem('classesList', jsonStr);
+
+    //different part for showing inside somewhere
+
+    document.getElementById("dataCLasses").setAttribute("href", "data:application/json," + JSON.stringify(obj));
+
+
+}
+
+function showClassJson() {
+    let classes = localStorage.getItem("classesList");
+
+    document.getElementById("printClassJson").innerHTML = '';
+
+    if (classes == undefined || classes == null) {
+        alert("You haven't yet entered any json content");
+    } else {
+        let stringClassesJson = JSON.stringify(JSON.parse(classes), null, 4); // Indented with tab
+        let textElement = document.createTextNode(stringClassesJson);
+        let preElement = document.createElement("pre");
+        preElement.appendChild(textElement);
+        document.getElementById("printClassJson").appendChild(preElement);
+    }
+
+
+}
+
+function removeClassesLocalStorage() {
+    let r = confirm("This will remove all Classes Created, Are you sure about this ?");
+    if (r == true) {
+        let classes = localStorage.getItem("classesList");
+        let stringInstructorJson = JSON.stringify(JSON.parse(classes), null, 4); // Indented with tab
+        let textElement = document.createTextNode(stringInstructorJson);
+        let preElement = document.createElement("pre");
+        preElement.appendChild(textElement);
+
+        let pElement = document.createElement("p");
+        let h1Element = document.createElement("h3");
+        let textInfo = document.createTextNode("Here is the deleted json :");
+        h1Element.appendChild(textInfo);
+        pElement.appendChild(h1Element);
+        document.getElementById("printClassJson").appendChild(pElement);
+
+        let pElement2= document.createElement("p");
+        pElement2.appendChild(preElement);
+        document.getElementById("printClassJson").appendChild(pElement2);
+
+        localStorage.removeItem("classesList");
+    }
+}
+//CLASSSES MON
 
 function removeLocalStorage() {
     let r = confirm("This will remove all of your instructors, Are you sure about this ?");
