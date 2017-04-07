7caa8a559292165c44794f70e38a48c7c2bc6ce1
arvince <alcaidearvince@gmail.com>
2017-04-08 00:19:37 +0800

    Add notes for Search Schedule

--- static/js/searchSchedule.js
+++ static/js/searchSchedule.js
@@ -1,45 +1,72 @@
 /**
  * Created by Arvince Neil on 05/04/2017.
  */
-function getContent(){
+function getContent() {
     let file = document.getElementById('importFile');
 
     if (file.files.length) {
         let reader = new FileReader();
 
         reader.onload = function (e) {
-            let allSchedule = e.target.result;
-            localStorage.setItem('InstructorJSON', allSchedule);
+            let allClasses = e.target.result;
+            localStorage.setItem('ourJsonObject', allClasses);
 
-            let jsonData = JSON.parse(allSchedule);
-        }
+            let jsonData = JSON.parse(allClasses);
+            alert(jsonData);
+            let nodeP = document.createElement("P");
+
+            for (let index in jsonData.classes) {
+                let eachClass = jsonData.classes[index];
+
+                let classUL = document.createElement("UL");
+                classUL.setAttribute("class", "schoolClassUL");
+
+                liAppender(eachClass.roomNo, classUL);
+                liAppender(eachClass.codeNo, classUL);
+                liAppender(eachClass.instructor, classUL);
+                liAppender(eachClass.subject, classUL);
+                liAppender(eachClass.locX, classUL);
+                liAppender(eachClass.locY, classUL);
+                liAppender(eachClass.classTime, classUL);
+                liAppender(eachClass.day, classUL);
+
+                checkBoxAppender(index, classUL);
+
+                nodeP.appendChild(classUL);
+                document.getElementById("currentClassesID").appendChild(nodeP);
+            }
+
+            updateChangesAppender("save changes", "saveTheChanges()", nodeP);
+
+        };
+
+        reader.readAsBinaryString(file.files[0]);
     }
 }
-
-function searchSchedule(){
-
-    let savedInfoLocalStorage = localStorage.getItem('InstructorJSON');
-
-    // if(!savedInfoLocalStorage){
-    //     alert("you have to provide the json file first ~");
-    //     return;
-    // }
-
-    let textVariable = document.getElementById("searchInput").value;
-    let classesJson = JSON.parse(savedInfoLocalStorage);
-
-    let codeNomArray  = new Array();
-
-    for(let index in classesJson.instructors){
-        let eachClass = classesJson.instructors[index]
-        let codeNo = eachClass.schedule;
-        codeNomArray.push(schedule);
-    }
-
-
-    let returnSchedule = searchStringInArray(textVariable, codeNomArray);
-
-    console.log(returnSchedule);
-
-}
+// function searchSchedule(){
+//
+//     let savedInfoLocalStorage = localStorage.getItem('scheduleJSON');
+//
+//     // if(!savedInfoLocalStorage){
+//     //     alert("you have to provide the json file first ~");
+//     //     return;
+//     // }
+//
+//     let textVariable = document.getElementById("searchInput").value;
+//     let classesJson = JSON.parse(savedInfoLocalStorage);
+//
+//     let codeNomArray  = new Array();
+//
+//     for(let index in classesJson.instructors){
+//         let eachClass = classesJson.instructors[index]
+//         let codeNo = eachClass.schedule;
+//         codeNomArray.push(schedule);
+//     }
+//
+//
+//     let returnSchedule = searchStringInArray(textVariable, codeNomArray);
+//
+//     console.log(returnSchedule);
+//
+// }
 
